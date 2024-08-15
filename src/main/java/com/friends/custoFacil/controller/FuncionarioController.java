3aa5b4a2836f808cfa4ea1c076045607cba6d0f8
erickwork <dery.erick@outlook.com>
2024-08-14 22:28:44 -0300

    #38 - Documentar endpoints

--- src/main/java/com/friends/custoFacil/controller/FuncionarioController.java
+++ src/main/java/com/friends/custoFacil/controller/FuncionarioController.java
@@ -7,6 +7,8 @@ import com.friends.custoFacil.dto.retornoFuncionarioID;
 import com.friends.custoFacil.dto.retornoFuncionarios;
 import com.friends.custoFacil.enums.StatusFuncionario;
 import com.friends.custoFacil.repository.FuncionarioRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +28,14 @@ public class FuncionarioController {
 
     @PostMapping
     @Transactional
+    @Operation(
+            summary = "Cadastra novo funcionario",
+            description = "Este endpoint permite criar um novo funcionario",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Funcionario criado com sucesso"),
+                    @ApiResponse(responseCode = "404", description = "Dados inválidos")
+            }
+    )
     public ResponseEntity<?> newFuncionario(@RequestBody @Valid CadastroFuncionario dados){
         try {
             funcionarioRepository.save(new Funcionario(dados));
@@ -36,11 +46,27 @@ public class FuncionarioController {
     }
 
     @GetMapping
+    @Operation(
+            summary = "Busca todos funcionario",
+            description = "Este endpoint permite busca todos os funcionarios independente do status",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Funcionarios encontrados"),
+                    @ApiResponse(responseCode = "404", description = "Nenhum funcionario foi encontrado")
+            }
+    )
     public List<retornoFuncionarios> getAllFuncionarios(){
         return funcionarioRepository.findAll().stream().map(retornoFuncionarios::new).toList();
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Busca um funcionario por ID",
+            description = "Este endpoint permite busca um funcionario pelo seu ID",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Funcionario encontrado"),
+                    @ApiResponse(responseCode = "404", description = "Nenhum funcionario foi encontrado")
+            }
+    )
     public ResponseEntity<?> getFuncionarioId(@PathVariable Long id){
         try {
             if(funcionarioRepository.findById(id).isPresent()){
@@ -55,6 +81,14 @@ public class FuncionarioController {
 
     @DeleteMapping("/{id}")
     @Transactional
+    @Operation(
+            summary = "Deletar um funcionario por ID",
+            description = "Este endpoint permite deletar um funcionario pelo seu ID",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Funcionario deletado"),
+                    @ApiResponse(responseCode = "404", description = "Nenhum funcionario foi encontrado")
+            }
+    )
     public ResponseEntity<?> inativarFuncionarioId(@PathVariable Long id){
         try{
             var funcionarioDados = funcionarioRepository.getReferenceById(id);
@@ -68,6 +102,14 @@ public class FuncionarioController {
 
     @PatchMapping("/{id}")
     @Transactional
+    @Operation(
+            summary = "Alterar um funcionario por ID",
+            description = "Este endpoint permite alterar um funcionario pelo seu ID",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Funcionario alterado"),
+                    @ApiResponse(responseCode = "404", description = "Nenhum funcionario foi encontrado")
+            }
+    )
     public ResponseEntity<?> reativarFuncionarioId(@PathVariable Long id){
         try{
             var funcionarioDados = funcionarioRepository.getReferenceById(id);
@@ -81,6 +123,14 @@ public class FuncionarioController {
 
 
     @GetMapping("/ativos")
+    @Operation(
+            summary = "Busca todos funcionario ativos",
+            description = "Este endpoint permite busca todos os funcionarios ativos",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Funcionarios encontrados"),
+                    @ApiResponse(responseCode = "404", description = "Nenhum funcionario foi encontrado")
+            }
+    )
     public List<Funcionario> getFuncionariosAtivos(){
         return funcionarioRepository.findByStatusFuncionario(statusFuncionario.ACTIVE);
     }
